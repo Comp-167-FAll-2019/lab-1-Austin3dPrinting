f1f33dd933f2e19c3644ed4f395c7145c9a6fce2
Austin George <austin@austingeorge.com>
2019-08-27 22:14:57 -0400

    Added comments, added getEmailAddress, revised ‘setNames’ & getPhoneNumber for future use and modularity

--- Contacts/src/contacts/Contact.java
+++ Contacts/src/contacts/Contact.java
@@ -22,16 +22,55 @@ public class Contact {
         this.emailAddress = emailAddress;
     }
     
+    // Gets First Name
     public String getFirstName(){
         
       return this.firstName;
     }
     
+    // Change First Name Method
+    public void setFirstName(String firstName){
+        
+        this.firstName = firstName;
+    }
+    
+    // Gets Last Name;
     public String getLastName(){
         
       return this.lastName;
     }
     
+    // Change Last Name Method
+    public void setLastName(String LastName){
+        
+        this.lastName = lastName;
+    }
+    
+    // Gets Phone Number;
+    public String getPhoneNumber(){
+        
+      return this.phoneNumber;
+    }
+    
+    // Change Phone Number Method
+    public void setPhoneNumber(String phoneNumber){
+        
+        this.phoneNumber = phoneNumber;
+    }
+    
+    // Gets Email Address;
+    public String getEmailAddress(){
+        
+      return this.emailAddress;
+    }
+    
+    // Change Email Address Method
+    public void setEmailAddress(String emailAddress){
+        
+        this.emailAddress = emailAddress;
+    }
+    
+    // Sends/Returns String of contact info
     public String toString(){
         return firstName + " " + lastName + " " + phoneNumber + " " + emailAddress;
     }
